9d25f93765ae2421b27c604ceea390fb56280a0e
Deepika <gjdeepika9@gmail.com>
2021-09-15 21:09:37 +0530

    Compute perimeter of rectangle

--- src/main/java/com/tw/ttt/rectangle/Rectangle.java
+++ src/main/java/com/tw/ttt/rectangle/Rectangle.java
@@ -2,10 +2,11 @@ package com.tw.ttt.rectangle;
 
 public class Rectangle {
 
-
     public double calculateArea(double length, double breadth) {
         return length * breadth;
     }
 
-
+    public double calculatePerimeter(double length, double breadth) {
+        return 2 * (length + breadth);
+    }
 }
--- src/test/java/com/tw/ttt/rectangletest/RectangleTest.java
+++ src/test/java/com/tw/ttt/rectangletest/RectangleTest.java
@@ -7,15 +7,21 @@ import org.junit.Test;
 public class RectangleTest {
 
     @Test
-    public void shouldReturnOneAsResult(){
+    public void shouldReturnOneAsArea() {
         Rectangle rectangle = new Rectangle();
-        Assert.assertEquals(1,rectangle.calculateArea(1.0,1.0),0);
+        Assert.assertEquals(1, rectangle.calculateArea(1.0, 1.0), 0);
     }
 
     @Test
-    public void shouldReturnAreaValue(){
+    public void shouldReturnAreaValue() {
         Rectangle rectangle = new Rectangle();
-        Assert.assertEquals(24.0,rectangle.calculateArea(8.0,3.0),0);
+        Assert.assertEquals(24.0, rectangle.calculateArea(8.0, 3.0), 0);
+    }
+
+    @Test
+    public void shouldReturnPerimeter() {
+        Rectangle rectangle = new Rectangle();
+        Assert.assertEquals(4.0, rectangle.calculatePerimeter(1.0, 1.0), 0);
     }
 
 }
